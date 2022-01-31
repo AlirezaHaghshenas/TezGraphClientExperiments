a9a6d1f6602b764dd112ac156521775e46d34e2f
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-01-31 09:56:48 -0800

    pagination, subscribe from level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo, gql, QueryRef } from 'apollo-angular';
-import { BlockNotification, BlockRecord, BlockRecordConnection } from 'src/tezgraph-types';
+import { map, Observable } from 'rxjs';
+import { BlockNotification, BlockRecordConnection } from 'src/tezgraph-types';
 import { BlockService } from './block.service';
 
 @Component({
@@ -11,7 +12,7 @@ import { BlockService } from './block.service';
 export class AppComponent implements OnInit {
   title = 'TezGraphClientExperiments2';
   result: any;
-  blocks?: BlockRecordConnection;
+  blocks: BlockRecordConnection[] = [];
   subscriptionBlocks: BlockNotification[] = [];
   lastBlockLevel?: number;
 
@@ -21,12 +22,52 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.apollo
+    this.getBlocks(null).subscribe((blocks) => {
+      this.lastBlockLevel = blocks.edges?.[0].node?.level;
+      if (this.lastBlockLevel) {
+        this.subscribe(this.lastBlockLevel + 1);
+      } else {
+        console.error(
+          'Could not determine block level to start subscription from'
+        );
+      }
+    });
+  }
+
+  subscribe(fromLevel: number) {
+    console.log('subscribed');
+    this.blocksService
+      .getSubscriptionAfterLevel(fromLevel)
+      .subscribe((result: any) => {
+        console.log('data arrived');
+        console.log(JSON.stringify(result));
+        const blockAdded = result.data.blockAdded as BlockNotification;
+        this.subscriptionBlocks.unshift(blockAdded);
+      });
+  }
+
+  loadMore() {
+    const nextCursor = this.blocks[this.blocks.length - 1].page_info.end_cursor;
+    this.getBlocks(nextCursor).subscribe(blocks => {
+      console.log('got next page')
+    });
+  }
+
+  getBlocks(cursor: string | null): Observable<BlockRecordConnection> {
+    return this.apollo
       .watchQuery({
         query: gql`
-          {
-            blocks(first: 10, order_by: { field: level, direction: desc }) {
+          query Blocks($after: Cursor) {
+            blocks(
+              first: 10
+              order_by: { field: level, direction: desc }
+              after: $after
+            ) {
               total_count
+              page_info {
+                has_next_page
+                end_cursor
+              }
               edges {
                 node {
                   hash
@@ -38,21 +79,13 @@ export class AppComponent implements OnInit {
           }
         `,
       })
-      .valueChanges.subscribe((result: any) => {
-        this.result = result;
-        this.blocks = result.data.blocks;
-        this.lastBlockLevel = this.blocks?.edges?.[0].node?.level;
-        this.subscribe();
-      });
-  }
-
-  subscribe() {
-    console.log('subscribed');
-    this.blocksService.subscribe().subscribe((result: any) => {
-      console.log('data arrived');
-      console.log(JSON.stringify(result));
-      const blockAdded = result.data.blockAdded as BlockNotification;
-      this.subscriptionBlocks.unshift(blockAdded);
-    });
+      .valueChanges.pipe(
+        map((result: any) => {
+          this.result = result;
+          const blocks = result.data.blocks as BlockRecordConnection;
+          this.blocks?.push(blocks);
+          return blocks;
+        })
+      );
   }
 }
--- src/app/block.service.ts
+++ src/app/block.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Subscription, gql } from 'apollo-angular';
+import { Subscription, gql, Apollo, TypedDocumentNode } from 'apollo-angular';
+import { EmptyObject } from 'apollo-angular/types';
+import { DocumentNode } from 'graphql';
 
 @Injectable({
   providedIn: 'root',
 })
-export class BlockService extends Subscription {
-  override document = gql`
+export class BlockService {
+  constructor(private readonly apollo: Apollo) {}
+
+  getSubscriptionAfterLevel(level: number) {
+    return new BlockSubscription(this.apollo, level).subscribe();
+  }
+}
+
+export class BlockSubscription extends Subscription {
+  override readonly document:
+    | DocumentNode
+    | TypedDocumentNode<any, EmptyObject>;
+
+  constructor(
+    public override readonly apollo: Apollo,
+    private readonly fromLevel: number
+  ) {
+    super(apollo);
+    this.document = gql`
     subscription {
-      blockAdded {
+      blockAdded(replayFromBlockLevel: ${fromLevel}) {
         hash
         header {
           level
@@ -16,4 +35,5 @@ export class BlockService extends Subscription {
       }
     }
   `;
+  }
 }
