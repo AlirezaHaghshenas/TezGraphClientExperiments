8f35f7f6c972949aed09630a5731eaaac5d8cf3a
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-03-17 09:43:24 -0700

    show token balance changes for selected operation

--- src/app/last-operations/last-operations.component.ts
+++ src/app/last-operations/last-operations.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
-import { map, Observable } from 'rxjs';
 import {
   OperationNotification,
+  OperationRecord,
   OperationRecordConnection,
 } from 'src/tezgraph-types';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Schema } from '@taquito/michelson-encoder';
 
 @Component({
   selector: 'app-last-operations',
@@ -18,7 +19,12 @@ export class LastOperationsComponent implements OnInit {
   operations: OperationRecordConnection[] = [];
   subscriptionBlocks: OperationNotification[] = [];
   source: string | null = null;
+  destination: string | null = null;
+  hash: string | null = null;
   isWorking = false;
+  operationDetails?: OperationRecord[];
+  message?: string;
+  tokenBalanceChanges?: any[];
   // lastBlockLevel?: number;
 
   get allOperations() {
@@ -37,39 +43,43 @@ export class LastOperationsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getOperations(null, this.source).subscribe((blocks) => {
-      // this.lastBlockLevel = blocks.edges?.[0].node?.level;
-      // if (this.lastBlockLevel) {
-      // this.subscribe(this.lastBlockLevel + 1);
-      // } else {
-      //   console.error(
-      //     'Could not determine block level to start subscription from'
-      //   );
-      // }
-    });
+    this.getOperations(null);
   }
 
   applyFilter() {
     this.operations = [];
-    this.getOperations(null, this.source).subscribe((_) => {});
+    this.getOperations(null);
   }
 
   loadMore() {
     const nextCursor =
       this.operations[this.operations.length - 1].page_info.end_cursor;
-    this.getOperations(nextCursor, this.source).subscribe((_) => {});
+    this.getOperations(nextCursor);
   }
 
-  getOperations(
-    cursor: string | null,
-    source: string | null
-  ): Observable<OperationRecordConnection> {
+  getOperations(cursor: string | null) {
     this.isWorking = true;
-    return this.apollo
-      .watchQuery({
+    this.message = undefined;
+    this.operationDetails = undefined;
+    this.tokenBalanceChanges = undefined;
+    this.apollo
+      .query({
         query: gql`
-          query Operations($after: Cursor, $source: Address) {
-            operations(first: 20, after: $after, filter: { source: $source }) {
+          query Operations(
+            $after: Cursor
+            $source: Address
+            $destination: Address
+            $hash: OperationHash
+          ) {
+            operations(
+              first: 20
+              after: $after
+              filter: {
+                source: $source
+                destination: $destination
+                hash: $hash
+              }
+            ) {
               page_info {
                 has_next_page
                 end_cursor
@@ -92,18 +102,196 @@ export class LastOperationsComponent implements OnInit {
         `,
         variables: {
           after: cursor,
-          source: !source ? undefined : source,
+          source: this.source ?? undefined,
+          destination: this.destination ?? undefined,
+          hash: this.hash ?? undefined,
         },
+        errorPolicy: 'ignore',
       })
-      .valueChanges.pipe(
-        map((result: any) => {
+      .subscribe({
+        next: (result: any) => {
           this.isWorking = false;
           this.result = result;
           const operations = result.data
             .operations as OperationRecordConnection;
           this.operations?.push(operations);
-          return operations;
-        })
-      );
+          if (result.errors) {
+            this.message = result.errors;
+          } else if (
+            !cursor &&
+            (this.source || this.destination) &&
+            !operations.edges
+          ) {
+            this.message = 'This account does not exist or is not used';
+          }
+        },
+        error: (error: any) => {
+          this.isWorking = false;
+          this.message = error;
+          console.log(error);
+        },
+      });
+  }
+
+  getDetails(operation: OperationRecord) {
+    this.isWorking = true;
+    this.operationDetails = undefined;
+    this.tokenBalanceChanges = undefined;
+    this.apollo
+      .query({
+        query: gql`
+          query OperationDetails($hash: OperationHash) {
+            operations(first: 100, filter: { hash: $hash }) {
+              edges {
+                node {
+                  kind
+                  hash
+                  batch_position
+                  internal
+                  block {
+                    hash
+                    level
+                    timestamp
+                  }
+                  source {
+                    address
+                  }
+                  bigmap_values(first: 100) {
+                    total_count
+                    page_info {
+                      has_next_page
+                      end_cursor
+                    }
+                    edges {
+                      node {
+                        kind
+                        key
+                        value
+                        contract {
+                          address
+                        }
+                        batch_position
+                        bigmap {
+                          id
+                          annots
+                          key_type
+                          value_type
+                        }
+                      }
+                    }
+                  }
+                  ... on DelegationRecord {
+                    fee
+                    counter
+                    gas_limit
+                    storage_limit
+                    amount
+                  }
+                  ... on EndorsementRecord {
+                    reward
+                    deposit
+                  }
+                  ... on OriginationRecord {
+                    fee
+                    counter
+                    gas_limit
+                    storage_limit
+                    balance
+                    delegate
+                    metadata {
+                      operation_result {
+                        status
+                        consumed_gas
+                        consumed_milligas
+                        errors
+                      }
+                    }
+                    contract_address
+                    storage_size
+                    burned
+                    contract {
+                      address
+                    }
+                  }
+                  ... on RevealRecord {
+                    fee
+                    counter
+                    gas_limit
+                    storage_limit
+                    metadata {
+                      operation_result {
+                        status
+                        consumed_gas
+                        consumed_milligas
+                        errors
+                      }
+                    }
+                  }
+                  ... on TransactionRecord {
+                    fee
+                    counter
+                    gas_limit
+                    storage_limit
+                    amount
+                    destination
+                    parameters {
+                      entrypoint
+                      value
+                      canonical_value
+                      michelson_value
+                    }
+                    metadata {
+                      operation_result {
+                        status
+                        consumed_gas
+                        consumed_milligas
+                        consumed_milligas
+                        errors
+                      }
+                    }
+                    storage_size
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          hash: operation.hash,
+        },
+      })
+      .subscribe({
+        next: (result: any) => {
+          this.isWorking = false;
+          const operations = (
+            result.data.operations as OperationRecordConnection
+          ).edges!.map((e) => e.node!);
+          this.operationDetails = operations;
+          this.extractInterestingInfo(operations);
+        },
+        error: (error: any) => {
+          this.isWorking = false;
+          this.message = error;
+        },
+      });
+  }
+
+  extractInterestingInfo(operations: OperationRecord[]) {
+    this.tokenBalanceChanges = operations
+      .flatMap((o) => o.bigmap_values.edges!)
+      ?.filter(
+        (e) =>
+          e.node?.bigmap.annots == '%ledger' ||
+          e.node?.bigmap.annots == '%balances'
+      )
+      .map((x) => {
+        const v = x.node;
+        const keyStorageSchema = new Schema(v?.bigmap.key_type);
+        const valueStorageSchema = new Schema(v?.bigmap.value_type);
+        return {
+          key: keyStorageSchema.Execute(v?.key),
+          value: valueStorageSchema.Execute(v?.value),
+        };
+      });
   }
 }
